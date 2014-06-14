5db01d6c1db7d62b0e78ad34e5cd9b8119c12422
gohawks8 <chuxu+github@hotmail.com>
2014-06-13 19:54:59 -0700

    Update cachematrix.R
    However, because of the limitation of Solve, it doesnt handle matrix that is non-square

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,38 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set <- function(y)
+  {
+    x<<-y
+    m<<-NULL    
+  }
+  
+  get <- function() x
+  setmatrix <- function(mysolve) m <<- mysolve
+  getmatrix <- function() m
+  list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # return a matrix that is the inverse of matrix 'x'
+  m <- x$getmatrix()
+  if (!is.null(m))
+  {
+    message("found cached matrix inverse..")
+    return (m)
+  }
+  else
+  {
+    # found cached matrix 
+    # get the matrix from cache
+    data <- x$get()
+    # Inverse it
+    m <- solve(data,...)
+    x$setmatrix(m)
+    m
+  }
 }
